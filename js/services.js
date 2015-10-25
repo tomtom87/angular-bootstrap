a98129402d4abeb63a9aa26f130f41be23e8b07c
tomtom87 <tom@gript.co.uk>
2015-10-25 14:08:49 +0000

    updating function name

--- js/services.js
+++ js/services.js
@@ -1,19 +1,19 @@
 
-function CatService($http) {
+function ThemeService($http) {
 
-	var CatService = {
+	var ThemeService = {
 		categories: []
 	};
 
-	CatService.getAllCategories = function() {
+	ThemeService.getAllCategories = function() {
     	//If they are already set, don't need to get them again
-		if (CatService.categories.length) {
+		if (ThemeService.categories.length) {
 			return;
 		}
 
 		//Get the category terms from wp-json
 		return $http.get('wp-json/taxonomies/category/terms').success(function(res){
-			CatService.categories = res;
+			ThemeService.categories = res;
 		});
 	};
 
@@ -21,4 +21,4 @@ function CatService($http) {
 }
 
 //Finally register the service
-app.factory('CatService', ['$http', CatService]);
+app.factory('ThemeService', ['$http', ThemeService]);
