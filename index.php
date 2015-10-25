79a8ced51069b4001cbaf8506978c8aae6019386
tomtom87 <tom@gript.co.uk>
2015-10-25 15:31:43 +0000

    Updating content display

--- index.php
+++ index.php
@@ -13,23 +13,11 @@
 
 get_header(); ?>
 
-	<div ng-view></div>
-
 	<?php if ( have_posts() ) : ?>
 
 		<?php /* Start the Loop */ ?>
-
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php
-				/* Include the Post-Format-specific template for the content.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'content', get_post_format() );
-			?>
-
-		<?php endwhile; ?>
+		
+		<div ng-view></div>
 
 		<?php _tk_content_nav( 'nav-below' ); ?>
 
--- js/scripts.js
+++ js/scripts.js
@@ -18,9 +18,12 @@ app.config(function($routeProvider, $locationProvider) {
 app.controller('Main', ['$scope', 'ThemeService', function($scope, ThemeService) {
 	//Get Categories from ThemeService
 	ThemeService.getAllCategories();
+	
 	//Get the first page of posts from ThemeService
 	ThemeService.getPosts(1);
+
 	$scope.data = ThemeService;
+
 }]);
 
 app.controller('Content',
